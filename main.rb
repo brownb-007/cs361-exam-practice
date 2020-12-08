723cfbb733b26848d28540f78a4e0884dd0aeee6
Brayden Brown <braydenbrown@gmail.com>
2020-12-07 19:38:44 -0800

    Changed formatting to fit ruby. Reference #3

--- main.rb
+++ main.rb
@@ -11,24 +11,9 @@ class Cube
   # Calculate the volume (w x h x d)
   def volume
         return @width * @height / @depth
-end
-end
+  end
 
+end
 
 cube = Cube.new(10, 20, 30)
 puts cube.volume
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
